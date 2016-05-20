9004c1943e52e729827a0c0aec52d56cd0d948e3
Luke <luke.szanto@gmail.com>
2016-05-20 14:05:35 +1000

    Revert claims

--- config/config.go
+++ config/config.go
@@ -2,7 +2,7 @@ package config
 
 // Config structure so we can access all attributes
 type Config struct {
-    DatabaseString string `json:"db_connection_string"`
-    DatabaseEngine string `json:"db_engine"`
-    SecretKey      string `json:"secret_key"`
+	DatabaseString string `json:"db_connection_string"`
+	DatabaseEngine string `json:"db_engine"`
+	SecretKey      string `json:"secret_key"`
 }
