4cf75f01c6edc903328b09911a48686fa5edfc1b
Luke <luke.szanto@gmail.com>
2016-05-24 13:48:38 +1000

    Try use react

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,42 +1,11 @@
-new Vue({
-    el: '#links-app',
-
-    http: {
-        root: '/api',
-        headers: {
-
-        }
-    },
-
-    data: function() {
-        return {
-            links: []
-        }
-    },
-
-    created: function() {
-        this.fetchLinks();
-    },
-
-    methods: {
-        fetchLinks: function() {
-            this.$http.get('links', function(data) {
-                this.links = data.links;
-            }.bind(this));
-        },
-
-        authoriseUser: function() {
-            this.$http.post('user/login', { 'username': 'luke', 'password': 'cool' }, function(data) {
-                alert('HELLO CAME BACK');
-            });
-        },
-
-        deleteLink: function(link) {
-            var resource = this.$resource('api/links/{id}');
-
-            resource.delete({ id: link.ID }, function(data) {
-                console.log(data);
-            }.bind(this));
-        }
+var LinksApp = React.createClass({
+    render: function() {
+        return (
+            <div className="links-app" >
+                Hello, World
+            </div>
+        );
     }
-})
+});
+
+ReactDOM.render(<LinksApp />, document.getElementById('app'));
