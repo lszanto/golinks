82089421de067405eeff42012c3e2a5e18f11675
lszanto <luke.szanto@gmail.com>
2016-09-17 23:55:50 +1000

    Add email field for creation

--- models/login.go
+++ models/login.go
@@ -4,4 +4,5 @@ package models
 type LoginUser struct {
 	Username string `json:"username" form:"username" binding:"required"`
 	Password string `json:"password" form:"password" binding:"required"`
+	Email    string `json:"email" form:"email"`
 }
