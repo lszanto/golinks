e032909678d8230270e9709cb7251c07bc07e95b
lszanto <luke.szanto@gmail.com>
2016-09-17 23:57:17 +1000

    Add json models so we can bind

--- models/link.go
+++ models/link.go
@@ -9,8 +9,8 @@ import (
 // Link structure, defines the link model
 type Link struct {
 	gorm.Model
-	URL       string
-	Title     string
+	URL       string     `json:"url" form:"url" binding:"required"`
+	Title     string     `json:"title" form:"title" binding:"required"`
 	DeletedAt *time.Time `json:",omitempty"`
 	User      *User      `json:",omitempty"`
 	UserID    int        `json:"-"`
